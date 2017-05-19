eccce449120588e3210ba3b3454bd2eee1ece288
qtechjin <sa615338@mail.ustc.edu.cn>
2017-05-19 10:52:51 +0800

    delete all to init new project

--- views/js/commons.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by liujin on 5/3/17.
- */
-function getProjectName() {
-    var pathName = location.pathname;
-    var projectName = pathName.substring(0, pathName.substr(1).indexOf("/")+1);
-    return projectName;
-}
